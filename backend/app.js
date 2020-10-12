fb8d373a54c69848a0366c4f31d8408c404fd774
nour karoui <nkaroui1998@gmail.com>
2020-10-12 09:00:21 +0100

    setting architecture

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+// change the db
+mongoose.connect('mongodb+srv://ostuser:ostuser@cluster0.mrzjp.mongodb.net/OSTteam?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('everything in place');
+  })
+  .catch(() => {
+    console.log('connection failed');
+  });
+
+app
+  .use(bodyParser.urlencoded({extended: true}))
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS');
+    next();
+  });
+
+module.exports = app;
