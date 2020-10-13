90a5c29d4e806ed78a83237ad916c2cfa9895765
bishkou <chedyhammami@yahoo.com>
2020-10-13 13:06:14 +0100

    client and addressClient done

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const clientRoutes = require('./routes/client')
+const addressClientRoutes = require('./routes/addressClient')
 const app = express();
 
 // change the db
@@ -20,6 +22,8 @@ app
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET, OPTIONS');
     next();
-  });
+  })
+  .use('/client', clientRoutes)
+  .use('/address', addressClientRoutes)
 
 module.exports = app;
