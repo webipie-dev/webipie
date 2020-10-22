b6bb2514756ba5373528c4ff343833d26ca40acc
PsycoCast <gabsiyassine@gmail.com>
2020-10-22 10:51:53 +0100

    fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
-    children:[
+    children: [
       {
         path: 'sales',
         component: SalesComponent
