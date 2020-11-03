3e73dab188748424665cfe3298a9a4ec1e791023
nour karoui <nkaroui1998@gmail.com>
2020-11-03 09:17:29 +0100

    fix issue

--- src/app/store-edit/social-media/social-media.component.ts
+++ src/app/store-edit/social-media/social-media.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-social-media',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SocialMediaComponent implements OnInit {
 
   constructor() { }
-
+  @Input() toggleS: () => void;
   ngOnInit(): void {
   }
 
