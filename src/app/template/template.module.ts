e37a4d60f90e3fcc6568cab967c4950f4d0aceb7
butterjack <jawher_bouhouch@yahoo.com>
2020-11-06 11:48:41 +0100

    add header and footer

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
 import {Ng2SmartTableModule} from "ng2-smart-table";
-import {ReactiveFormsModule} from '@angular/forms';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {TemplateRoutingModule} from './template-routing.module';
 import { BannerImageComponent } from './banner-image/banner-image.component';
 import {TemplateComponent} from './template.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 
 @NgModule({
@@ -14,11 +17,14 @@ import {TemplateComponent} from './template.component';
     HttpClientModule,
     Ng2SmartTableModule,
     ReactiveFormsModule,
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [
     BannerImageComponent,
-    TemplateComponent
+    TemplateComponent,
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class TemplateModule {
