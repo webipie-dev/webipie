83d87f1d84203c381c2f92d17e76f4e34684baa3
nour-karoui <nkaroui1998@gmail.com>
2020-11-06 11:16:04 +0100

    product card

--- src/app/template/product-card/product-card.component.ts
+++ src/app/template/product-card/product-card.component.ts
@@ -1,21 +1,45 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, HostBinding} from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.css']
+  styleUrls: ['./product-card.component.css'],
+  animations: [
+    trigger('onHover', [
+      state('hover', style({
+          transform: 'scale(1.5)'
+        })
+      ),
+      transition('fixed => hover', [
+        animate('0.8s')
+      ])
+    ])
+  ]
 })
 export class ProductCardComponent implements OnInit {
 
+  hover = false;
+  fixed = true;
   @Input()src: string;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  changeImg(state) {
-    if (state === 'enter') {
-      this.src = '../../../assets/images/fashion-WPWVGRY.jpg'
+  onImgHover() {
+    this.hover = !this.hover;
+    this.fixed = !this.fixed;
+    if (this.hover) {
+      this.src = '../../../assets/images/fashion-WPWVGRY.jpg';
+    } else {
+      this.src = '../../../assets/images/dress.jpg';
     }
   }
 
--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
-import {HttpClientModule} from "@angular/common/http";
-import {Ng2SmartTableModule} from "ng2-smart-table";
+import {HttpClientModule} from '@angular/common/http';
+import {Ng2SmartTableModule} from 'ng2-smart-table';
 import {ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {TemplateRoutingModule} from './template-routing.module';
@@ -8,6 +8,7 @@ import { BannerImageComponent } from './banner-image/banner-image.component';
 import {TemplateComponent} from './template.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import {ChangeImgDirective} from './product-card/change-img.directive';
+
 
 @NgModule({
   imports: [
