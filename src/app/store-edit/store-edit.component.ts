cda8bf7da5d253754f7e09dc61244e9a1936b062
PsycoCast <gabsiyassine@gmail.com>
2020-11-12 12:22:29 +0100

    preview template

--- src/app/store-edit/store-edit.component.ts
+++ src/app/store-edit/store-edit.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-store-edit',
@@ -7,14 +8,25 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 
 export class StoreEditComponent implements OnInit {
-
-  constructor() { }
+  urlToPreview: string = "http://localhost:4200/";
+  urlSafe: SafeResourceUrl;
+  constructor(public sanitizer: DomSanitizer) { }
   toggleS = (): void =>  {
     console.log(document.getElementById("sidebar"));
     document.getElementById("sidebar").classList.toggle("active");
     document.getElementById("sidebar-non-active").classList.toggle("active");
     document.getElementById("sidebar-next").classList.toggle("sidenav-next");
+    var event = new CustomEvent("resize");
+    document.dispatchEvent(event);
   }
   ngOnInit(): void {
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.urlToPreview);
+    var newWidth = window.screen.width - document.getElementById('sidebar').offsetWidth + "px";
+    document.getElementById("iframe").style.width = newWidth.toString();
+    window.addEventListener("resize", () => {
+      var newWidth = window.screen.width - document.getElementById('sidebar').offsetWidth + "px";
+      document.getElementById("iframe").style.width = newWidth.toString();
+    });
+
   }
 }
