fc390b4281fe4fb7d9020cb410b08c044fa1f2c2
PsycoCast <gabsiyassine@gmail.com>
2020-11-14 13:42:04 +0100

    .

--- src/app/store-edit/store-edit.component.ts
+++ src/app/store-edit/store-edit.component.ts
@@ -10,22 +10,43 @@ import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 export class StoreEditComponent implements OnInit {
   urlToPreview: string = "http://localhost:4200/";
   urlSafe: SafeResourceUrl;
+  windowHeight = window.innerHeight;
+  newWidth;
   constructor(public sanitizer: DomSanitizer) { }
   toggleS = (): void =>  {
     console.log(document.getElementById("sidebar"));
+    console.log(document.getElementById("sidebar-non-active"));
+    console.log(document.getElementById("sidebar-next"));
     document.getElementById("sidebar").classList.toggle("active");
     document.getElementById("sidebar-non-active").classList.toggle("active");
-    document.getElementById("sidebar-next").classList.toggle("sidenav-next");
+    document.getElementById('sidebar-non-active2').classList.toggle('hidden-sidenav');
+    document.getElementById('iframe').classList.toggle('margin-iframe');
+    document.getElementById('iframe').style.width = this.newWidth.toString();
+    if (document.getElementById('sidebar').classList.contains('active')) {
+      this.newWidth = window.screen.width - document.getElementById('sidebar-non-active').offsetWidth + 'px';
+    } else {
+      this.newWidth = window.screen.width - document.getElementById('sidebar').offsetWidth + 'px';
+    }
+    document.getElementById('iframe').style.width = this.newWidth.toString();
     var event = new CustomEvent("resize");
+    console.log(event);
     document.dispatchEvent(event);
   }
   ngOnInit(): void {
     this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.urlToPreview);
-    var newWidth = window.screen.width - document.getElementById('sidebar').offsetWidth + "px";
-    document.getElementById("iframe").style.width = newWidth.toString();
-    window.addEventListener("resize", () => {
-      var newWidth = window.screen.width - document.getElementById('sidebar').offsetWidth + "px";
-      document.getElementById("iframe").style.width = newWidth.toString();
+    if (document.getElementById('sidebar').classList.contains('active')) {
+     this.newWidth = window.screen.width - document.getElementById('sidebar-non-active').offsetWidth + 'px';
+    } else {
+      this.newWidth = window.screen.width - document.getElementById('sidebar').offsetWidth + 'px';
+    }
+    document.getElementById('iframe').style.width = this.newWidth.toString();
+    window.addEventListener('resize', () => {
+      if (document.getElementById('sidebar').classList.contains('active')) {
+        this.newWidth = window.screen.width - document.getElementById('sidebar-non-active').offsetWidth + 'px';
+      } else {
+        this.newWidth = window.screen.width - document.getElementById('sidebar').offsetWidth + 'px';
+      }
+      document.getElementById('iframe').style.width = this.newWidth.toString();
     });
 
   }
