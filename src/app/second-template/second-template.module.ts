5dcce8a93bdfb6cf5243d03474a22fd8b5799b0a
PsycoCast <gabsiyassine@gmail.com>
2021-01-17 19:37:42 +0100

    begin integration

--- src/app/second-template/second-template.module.ts
+++ src/app/second-template/second-template.module.ts
@@ -4,11 +4,15 @@ import { HeaderSecondTemplateComponent } from './header-second-template/header-s
 import { FooterSecondTemplateComponent } from './footer-second-template/footer-second-template.component';
 import { HomeSecondTemplateComponent } from './home-second-template/home-second-template.component';
 import {SecondTemplateRoutingModule} from "./second-template-routing.module";
+import { TopSliderComponent } from './top-slider/top-slider.component';
+import { ProductsSectionSecondTemplateComponent } from './products-section-second-template/products-section-second-template.component';
+import { AboutSecondTemplateComponent } from './about-second-template/about-second-template.component';
+import { ProductsAllSecondTemplateComponent } from './products-all-second-template/products-all-second-template.component';
 
 
 
 @NgModule({
-  declarations: [HeaderSecondTemplateComponent, FooterSecondTemplateComponent, HomeSecondTemplateComponent],
+  declarations: [HeaderSecondTemplateComponent, FooterSecondTemplateComponent, HomeSecondTemplateComponent, TopSliderComponent, ProductsSectionSecondTemplateComponent, AboutSecondTemplateComponent, ProductsAllSecondTemplateComponent],
   exports: [
     HeaderSecondTemplateComponent,
     FooterSecondTemplateComponent
