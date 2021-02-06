c24832ce46f0d798897b95aa307ce1541cb23e5a
bishkou <chedyhammami@yahoo.com>
2021-02-06 19:38:26 +0100

    fixed font shadi

--- src/app/store-edit/change-font/change-font.component.ts
+++ src/app/store-edit/change-font/change-font.component.ts
@@ -16,24 +16,12 @@ export class ChangeFontComponent implements OnInit {
   /*
     general settings to any template
   */
-  fontTypes: Array<string>;
-  fontSizeMin: number;
-  fontSizeMax: number;
-  fontWeights: Array<string>;
-  // const alignments = ["center", "right", "left"];
-
+  fontTypes: Array<string> = JSON.parse(sessionStorage.getItem('store')).template.fontOptions;
   /*
     specific settings to user's template
   */
 
-  fontType: string;
-  fontSize: number;
-  fontWeight: string;
-  textShadow: boolean;
-  textShadowColor: string;
-  textAlign: string;
-  textBold: boolean;
-  textItalic: boolean;
+  fontType = JSON.parse(sessionStorage.getItem('store')).template.font;
 
   storeId = JSON.parse(sessionStorage.getItem('store'))._id;
 
@@ -43,112 +31,13 @@ export class ChangeFontComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.fontTypes = [
-      'Serif',
-      'Sans-serif',
-      'Monospace',
-      'Cursive'
-    ];
-    this.fontSizeMin = 5;
-    this.fontSizeMax = 50;
-    this.fontWeights = [
-      'normal',
-      'bold',
-      'bolder',
-      'lighter'
-    ];
-    // const alignments = ["center", "right", "left"];
 
-    this.fontType = 'Serif';
-    this.fontSize = 10;
-    this.fontWeight = 'normal';
-    this.textShadow = false;
-    this.textShadowColor = 'blue';
-    this.textAlign = 'center';
-    this.textBold = false;
-    this.textItalic = false;
-  }
-
-  sizeChange(value): void {
-    console.log('the size value is' + value);
-    this.fontSize = value;
-  }
-
-  shadowChange(value): void {
-    this.textShadow = (value === '1');
-  }
-
-  alignChange(event, value): void {
-
-    let target = event.target;
-    if (event.target.tagName === 'path') {
-      target = event.target.parentNode;
-    }
-
-    console.log(target);
-    // add the css effects
-    target.classList.add('alignment');
-
-    const icons = document.querySelectorAll('.alignment');
-
-    // tslint:disable-next-line:prefer-for-of
-    for (let i = 0; i < icons.length; i++) {
-
-      // If the icon is the one clicked, skip it
-      if (icons[i] === event.target) {
-        continue;
-      }
-
-      // Remove the .alignment class
-      icons[i].classList.remove('alignment');
-
-    }
-
-    this.textAlign = value;
-  }
-
-  onSelect(event, value): void {
-    let target = event.target;
-    if (event.target.tagName === 'path') {
-      target = event.target.parentNode;
-    }
-
-    if (value === 'font-size') {
-      this.fontSize += 2;
-      return;
-    }
-
-    target.classList.toggle('font-item-selected');
-
-    if (value === 'italic') {
-      this.textItalic = !this.textItalic;
-    }
-  }
-
-  submit(): void {
-    const postData = {
-      'template.font.name': this.fontType,
-      'template.font.size': this.fontSize,
-      'template.font.weight': this.fontWeight,
-      'template.font.alignment': this.textAlign,
-      'template.font.bold': this.textBold,
-      'template.font.italic': this.textItalic,
-
-    };
-    this.storeService.edit(this.storeId, postData).subscribe(store => {
-      sessionStorage.setItem('store', JSON.stringify(store));
-    });
   }
 
   onSubmit(): void {
     const postData = {
       ids: this.storeId,
       type: this.fontType,
-      size: this.fontSize,
-      weight: this.fontWeight,
-      alignment: this.textAlign,
-      bold: this.textBold,
-      italic: this.textItalic,
 
     };
     this.storeService.edit(this.storeId, postData).subscribe(store => {
@@ -160,3 +49,61 @@ export class ChangeFontComponent implements OnInit {
   }
 
 }
+
+
+//
+// sizeChange(value): void {
+//   console.log('the size value is' + value);
+//   this.fontSize = value;
+// }
+//
+// shadowChange(value): void {
+//   this.textShadow = (value === '1');
+// }
+//
+// alignChange(event, value): void {
+//
+//   let target = event.target;
+//   if (event.target.tagName === 'path') {
+//   target = event.target.parentNode;
+// }
+//
+// console.log(target);
+// // add the css effects
+// target.classList.add('alignment');
+//
+// const icons = document.querySelectorAll('.alignment');
+//
+// // tslint:disable-next-line:prefer-for-of
+// for (let i = 0; i < icons.length; i++) {
+//
+//   // If the icon is the one clicked, skip it
+//   if (icons[i] === event.target) {
+//     continue;
+//   }
+//
+//   // Remove the .alignment class
+//   icons[i].classList.remove('alignment');
+//
+// }
+//
+// this.textAlign = value;
+// }
+//
+// onSelect(event, value): void {
+//   let target = event.target;
+//   if (event.target.tagName === 'path') {
+//   target = event.target.parentNode;
+// }
+//
+// if (value === 'font-size') {
+//   this.fontSize += 2;
+//   return;
+// }
+//
+// target.classList.toggle('font-item-selected');
+//
+// if (value === 'italic') {
+//   this.textItalic = !this.textItalic;
+// }
+// }
