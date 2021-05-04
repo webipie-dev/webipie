ca1b0b2188d77c16c772dce05d5000f8d3b269d7
Alaeddine Abdessalem <alaeddine-13@live.fr>
2021-05-04 02:33:31 +0100

    Use the correct http protocol

--- src/app/dashboard/header/header.component.ts
+++ src/app/dashboard/header/header.component.ts
@@ -6,6 +6,7 @@ import {encryptLocalStorage} from '../../_shared/utils/encrypt-storage';
 import { WebSocketService } from 'src/app/_shared/services/web-socket.service';
 import { NgZone } from '@angular/core';
 import {environment} from '../../../environments/environment';
+const { httpProtocol, hostname, port } = require('./configuration');
 
 declare var $;
 
@@ -96,7 +97,7 @@ export class HeaderComponent implements OnInit {
   preview() {
     const storeId = encryptLocalStorage.decryptString(localStorage.getItem('storeID'));
     this.storeService.getById(storeId).subscribe(store => {
-      const url = 'http://' + store.url + environment.urlToPreview;
+      const url = `${httpProtocol}://${store.url}:${port}`;
       window.open(url, '_blank');
     });
   }
